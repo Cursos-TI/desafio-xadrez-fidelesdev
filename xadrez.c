eb45a7dce412fa7b0bc3def7ca8179f2f2b8e9cb
SK3010z <sk3010mfc@gmail.com>
2025-04-08 21:11:30 -0300

    xadrez nivel novato

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,40 @@
 #include <stdio.h>
+#include <string.h>
 
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    short int escolha;
+    do{
+        printf("------Movimentacoes de pecas de xadrez------\n");
+        printf("1.Torre\n");
+        printf("2.Bispo\n");
+        printf("3.Rainha\n");
+        scanf("%hd", &escolha);
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+        switch(escolha){
+            case 1:
+                for(int i = 0; i < 5; i++){
+                    printf("Direita\n");
+                }
+                break;
+            case 2:
+                for(int i = 0; i < 5; i++){
+                    printf("Cima, Direita\n");
+                }
+                break;
+            case 3:
+                for(int i = 0; i < 8; i++){
+                    printf("Esquerda\n");
+                }
+                break;
+            default:
+                printf("Opcao invalida, digite novamente:\n");
+                break;
+        }
+    } while(escolha < 1 || escolha > 3);
 
     return 0;
 }
